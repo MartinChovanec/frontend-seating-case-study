2a87f9f5391092c1963c4bc1570594da15cb360c
Martin Chovanec <chovanec.m11@gmail.com>
2025-01-29 04:18:10 +0100

    React Router imported

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { CartProvider } from "@/components/context/CartContext";
+import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
     <CartProvider>
         <React.StrictMode>
             <App />
         </React.StrictMode>
         ,
     </CartProvider>
+    </BrowserRouter>
 );
