2da350da3db57d4a2a1c87ed10e7421589b5ffd0
Martin Chovanec <chovanec.m11@gmail.com>
2025-02-02 18:22:42 +0100

    Header.tsx created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import { useState } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu.tsx";
 import "./App.css";
 import { getEvent } from "@/components/hooks/GetEvent";
 import { getSeats } from "@/components/hooks/GetSeats";
@@ -21,20 +11,18 @@ import LoginModal from "@/components/LoginModal";
 import OrderConfirmation from "@/components/OrderConfirmation";
 import { SeatingMap } from "@/components/SeatingMap";
 import { EventInfoPanel } from "@/components/EventInfoPanel";
+import { Header } from "./components/Header";
+import { UserData } from "./types/types";
 
-interface UserData {
-    email: string;
-    firstName: string;
-    lastName: string;
-  }
+
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const [user, setUser] = useState<UserData | null>(null);
 
     const { event, loading, error } = getEvent();
 
-    // Fetch seat data
     const { seats, loading: seatsLoading, error: seatsError } = getSeats(event?.eventId || "");
 
     const { cart } = useCart();
@@ -53,55 +41,8 @@ function App() {
                 element={
                     <div className="flex flex-col grow">
                         {/* header (wrapper) */}
-                        <nav className="sticky top-0 left-0 right-0 bg-white border-b border-zinc-200 flex justify-center">
-                            {/* inner content */}
-                            <div className="max-w-screen-lg p-4 grow flex items-center justify-between gap-3">
-                                {/* application/author image/logo placeholder */}
-                                <div className="max-w-[250px] w-full flex">
-                                    <div className="bg-zinc-100 rounded-md size-12" />
-                                </div>
-                                {/* app/author title/name placeholder */}
-                                <div className="bg-zinc-100 rounded-md h-8 w-[200px]" />
-                                {/* user menu */}
-                                <div className="max-w-[250px] w-full flex justify-end">
-                                    {isLoggedIn && user ? (
-                                        <DropdownMenu>
-                                            <DropdownMenuTrigger asChild>
-                                                <Button variant="ghost">
-                                                    <div className="flex items-center gap-2">
-                                                        <Avatar>
-                                                            <AvatarImage
-                                                                src={`https://source.boringavatars.com/marble/120/<user-email>?colors=25106C,7F46DB`}
-                                                            />
-                                                            <AvatarFallback>{user.firstName[0]} {user.lastName[0]}</AvatarFallback>
-                                                        </Avatar>
-
-                                                        <div className="flex flex-col text-left">
-                                                            <span className="text-sm font-medium">{user.firstName} {user.lastName}</span>
-                                                            <span className="text-xs text-zinc-500">
-                                                            {user.email}
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </Button>
-                                            </DropdownMenuTrigger>
-                                            <DropdownMenuContent className="w-[250px]">
-                                                <DropdownMenuLabel>{user.firstName} {user.lastName}</DropdownMenuLabel>
-                                                <DropdownMenuSeparator />
-                                                <DropdownMenuGroup>
-                                                    <DropdownMenuItem disabled>Logout</DropdownMenuItem>
-                                                </DropdownMenuGroup>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
-                                    ) : (
-                                        <Button disabled variant="secondary">
-                                            Login or register
-                                        </Button>
-                                    )}
-                                </div>
-                            </div>
-                        </nav>
-
+                        
+                        <Header user={user} isLoggedIn={isLoggedIn}/>
                         {/* main body (wrapper) */}
                         <main className="grow flex flex-col justify-center">
                             {/* inner content */}
---
+++ src/components/Header.tsx
@@ -0,0 +1,74 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu.tsx";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { UserData } from "@/types/types";
+
+
+
+interface HeaderProps {
+    user: UserData | null;
+    isLoggedIn : boolean;
+}
+
+
+
+export const Header: React.FC<HeaderProps> = ({user, isLoggedIn}) => {
+    return (
+<nav className="sticky top-0 left-0 right-0 bg-white border-b border-zinc-200 flex justify-center">
+                            {/* inner content */}
+                            <div className="max-w-screen-lg p-4 grow flex items-center justify-between gap-3">
+                                {/* application/author image/logo placeholder */}
+                                <div className="max-w-[250px] w-full flex">
+                                    <div className="bg-zinc-100 rounded-md size-12" />
+                                </div>
+                                {/* app/author title/name placeholder */}
+                                <div className="bg-zinc-100 rounded-md h-8 w-[200px]" />
+                                {/* user menu */}
+                                <div className="max-w-[250px] w-full flex justify-end">
+                                    {isLoggedIn && user ? (
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button variant="ghost">
+                                                    <div className="flex items-center gap-2">
+                                                        <Avatar>
+                                                            <AvatarImage
+                                                                src={`https://source.boringavatars.com/marble/120/<user-email>?colors=25106C,7F46DB`}
+                                                            />
+                                                            <AvatarFallback>{user.firstName[0]} {user.lastName[0]}</AvatarFallback>
+                                                        </Avatar>
+
+                                                        <div className="flex flex-col text-left">
+                                                            <span className="text-sm font-medium">{user.firstName} {user.lastName}</span>
+                                                            <span className="text-xs text-zinc-500">
+                                                            {user.email}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent className="w-[250px]">
+                                                <DropdownMenuLabel>{user.firstName} {user.lastName}</DropdownMenuLabel>
+                                                <DropdownMenuSeparator />
+                                                <DropdownMenuGroup>
+                                                    <DropdownMenuItem disabled>Logout</DropdownMenuItem>
+                                                </DropdownMenuGroup>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    ) : (
+                                        <Button disabled variant="secondary">
+                                            Login or register
+                                        </Button>
+                                    )}
+                                </div>
+                            </div>
+                        </nav>
+    )
+}
