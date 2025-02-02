2d67ce3e27e438b25cd15ff0575116be23099803
Martin Chovanec <chovanec.m11@gmail.com>
2025-02-02 02:46:55 +0100

    folder with types created

---
+++ src/types/types.ts
@@ -0,0 +1,48 @@
+export interface TicketType {
+    id: string;
+    name: string;
+    price: number;
+}
+
+export interface Seat {
+    seatId: string;
+    place: number;
+    information: string;
+    ticketTypeId?: string;
+}
+
+export interface SeatRow {
+    seatRow: number;
+    seats: Seat[];
+}
+
+export interface SeatData {
+    ticketTypes: TicketType[];
+    seatRows: SeatRow[];
+}/*
+
+
+export interface EventData {
+    eventId: string;
+    namePub: string;
+    description: string;
+    currencyIso: string;
+    dateFrom: string;
+    dateTo: string;
+    headerImageUrl: string;
+    place: string;
+}
+
+export interface CartItem {
+    seatId: string;
+    row: number;
+    place: number;
+    price?: number;
+    ticketType?: string;
+}
+*/
+export interface UserData {
+    email: string;
+    firstName: string;
+    lastName: string;
+} 
